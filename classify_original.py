593905ac3a1a0db123b546bcb89da20fb7086a4a
Alpha Wave Technologies <80882754+alphawave1@users.noreply.github.com>
2021-03-25 15:55:43 +0530

    Add files via upload

--- classify_original.py
+++ classify_original.py
@@ -6,14 +6,16 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
+image_path = "testing.png"
 
-def analyse(imageObj):
+if image_path:
+    
     # Read the image_data
-    image_data = tf.gfile.FastGFile(imageObj, 'rb').read()
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line 
-                        in tf.gfile.GFile("tf_files/retrained_labels.txt")]
+                       in tf.gfile.GFile("tf_files/retrained_labels.txt")]
 
     # Unpersists graph from file
     with tf.gfile.FastGFile("tf_files/retrained_graph.pb", 'rb') as f:
@@ -26,14 +28,12 @@ def analyse(imageObj):
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         
         predictions = sess.run(softmax_tensor, \
-                    {'DecodeJpeg/contents:0': image_data})
+                 {'DecodeJpeg/contents:0': image_data})
         
         # Sort to show labels of first prediction in order of confidence
         top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-        obj = {}
+        
         for node_id in top_k:
             human_string = label_lines[node_id]
             score = predictions[0][node_id]
-            obj[human_string] = float(score)
-        
-        return obj
+            print('%s (score = %.5f)' % (human_string, score))
